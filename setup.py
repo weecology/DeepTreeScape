c1eadd901f7b06e71577a61e30b1bf214bee080d
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-04 15:09:12 -0800

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Joint point cloud and RGB individual tree modeling',
+    author='Ben Weinstein',
+    license='MIT',
+)
